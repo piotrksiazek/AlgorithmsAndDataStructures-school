07a90fa08154994f3fd62e89d360d73119846c58
Piotr <pksiazek.py@gmail.com>
2022-03-14 13:47:46 +0100

    bitset insert, remove, contains

---
+++ bitset/bitset.hpp
@@ -0,0 +1,97 @@
+#include "../set/LinkedList.hpp"
+#include "../set/Node.hpp"
+#include <bitset>
+using namespace std;
+
+template<int min, int max>
+class BitSet
+{
+    public:
+        BitSet()
+        {
+            this->size = 0;
+            int numberOfElements = max - min + 1;
+            this->numberOfBytes = numberOfElements / 8;
+            this->tab = new unsigned char[this->numberOfBytes];
+        }
+
+        ~BitSet()
+        {
+            delete[] this->tab;
+        }
+
+        int getTabIndex(int value) const
+        {
+            return value / 8;
+        }
+
+        int getBitShift(int value, int tabIndex) const
+        {
+            return (value - tabIndex * 8);
+        }
+
+        void changeSizeCount(unsigned char oldByte, unsigned char newByte)
+        {
+            if(oldByte != newByte)
+            {
+                this->size++;
+            }
+        }
+        
+        void insert(int value)
+        {
+            int tabIndex = this->getTabIndex(value);
+            tab[tabIndex] |= (1 << getBitShift(value, tabIndex));
+        }
+
+        void remove(int value)
+        {
+            int tabIndex = this->getTabIndex(value);
+            tab[tabIndex] &= ~(1 << getBitShift(value, tabIndex));
+        }
+
+        bool contains(int value) const
+        {
+            int tabIndex = this->getTabIndex(value);
+            unsigned char byte = tab[tabIndex];
+            return (byte & (1 << this->getBitShift(value, tabIndex))) > 0;
+        }
+
+        int size()
+        {
+            
+        }
+
+        // void Union(const set &other)
+        // {
+           
+        // }
+
+        // void intersection(const set &other)
+        // {
+            
+        // }
+
+        // void difference(const set &other)
+        // {
+            
+        // }
+
+        // bool operator==(set& l2)
+        // {
+            
+        // }
+
+        void print()
+        {
+            for(int i = 0; i < this->numberOfBytes; i++)
+            {
+                cout << bitset<8>(this->tab[i]) << " ";
+            }
+            cout<<endl;
+        }
+    private:
+        unsigned char *tab;
+        int numberOfBytes;
+        int size;
+};
---
+++ bitset/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "bitset.hpp"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int result = 15 - 8;
+    cout << result << endl;
+
+    BitSet<0,15> *set1 = new BitSet<0,15>();
+
+    set1->insert(1);
+    set1->insert(7);
+    set1->insert(8);
+
+    bool contains1 = set1->contains(1);
+    bool contains7 = set1->contains(7);
+    bool contains10 = set1->contains(10);
+    bool contains8 = set1->contains(8);
+
+    cout<<"contains1: "<<contains1<<endl;
+    cout<<"contains7: "<<contains7<<endl;
+    cout<<"contains10: "<<contains10<<endl;
+    cout<<"contains8: "<<contains8<<endl;
+
+
+    set1->print();
+}
